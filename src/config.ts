e23213c1cd068c54ae19ace2daec6b165bd41fe9
dogelana <team@dogelana.com>
2023-01-09 03:11:19 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,14 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 
 export const network = (process.env.NEXT_PUBLIC_SOLANA_NETWORK ||
-  WalletAdapterNetwork.Devnet) as WalletAdapterNetwork;
-// const network = WalletAdapterNetwork.Devnet;
+  WalletAdapterNetwork.Mainnet) as WalletAdapterNetwork;
+
 export const rpcHost =
-  process.env.NEXT_PUBLIC_RPC_HOST || clusterApiUrl(network);
+  "https://ssc-dao.genesysgo.net";
 
 export const candyMachineId = new PublicKey(
   process.env.NEXT_PUBLIC_CANDY_MACHINE_ID ||
-    "EmS5C7XnZV2JQzHDdUe1TD6ZqXDrXnxQ8DLF9rYA9MEq"
+    "5ncMWxnT9Ai679DdZen1zXAnmCa2kXN5pgd8UYaQ6qUH"
 );
 export const defaultGuardGroup =
   process.env.NEXT_PUBLIC_DEFAULT_GUARD_GROUP || undefined;
