9d04d497353fdd795fb6c684e6c8ba05131ec237
dogelana <team@dogelana.com>
2023-03-21 19:46:09 -0400

    fix

--- src/Home.tsx
+++ src/Home.tsx
@@ -382,7 +382,7 @@ const Home = (props: HomeProps) => {
                         gatekeeperNetwork={guards.gatekeeperNetwork}
                         connection={connection}
                         cluster={
-                          process.env.NEXT_PUBLIC_SOLANA_NETWORK || "mainnet"
+                          process.env.NEXT_PUBLIC_SOLANA_NETWORK || "devnet"
                         }
                         options={{ autoShowModal: false }}
                       >
