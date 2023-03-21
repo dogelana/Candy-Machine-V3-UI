0ae8279ca7a617da374394d99f00bd89066b7ac2
dogelana <team@dogelana.com>
2023-03-21 18:39:25 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const network = (process.env.NEXT_PUBLIC_SOLANA_NETWORK ||
   WalletAdapterNetwork.Mainnet) as WalletAdapterNetwork;
 
 export const rpcHost =
-  "https://fittest-bold-smoke.solana-mainnet.discover.quiknode.pro/724c0f3ca62c79d270f7dcf47583e3591e0a9b31/";
+  "https://api.mainnet-beta.solana.com";
 
 export const candyMachineId = new PublicKey(
   process.env.NEXT_PUBLIC_CANDY_MACHINE_ID ||
